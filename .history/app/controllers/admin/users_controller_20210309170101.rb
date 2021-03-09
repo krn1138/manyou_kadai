2f18bd2d38b0a7d0f51a2782484a4bf61bb23d2b
oomiya karen <krn971138@gmail.com>
2021-03-09 22:34:09 +0900

    管理画面実装完了

---
+++ .history/app/controllers/admin/users_controller_20210309170101.rb
@@ -0,0 +1,65 @@
+class Admin::UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :admin_user, only: %i[index new show edit destroy]
+
+
+  def index
+    @users = User.includes(:tasks).all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # session[:user_id] = @user.id
+      redirect_to user_path(@user.id), notice:"ユーザー#{@user.name}を登録しました"
+      # redirect_to admin_user_path, notice:"ユーザー#{@user.name}を登録しました"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: "ユーザー#{@user.name}を更新しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to admin_users_path, notice: "ユーザー#{@user.name}を削除しました"
+    else 
+      redirect_to admin_users_path, notice: "#{@user.name}を削除できませんでした"
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(
+      :user
+    ).permit(
+      :name, 
+      :email, 
+      :admin, 
+      :password,
+      :password_confirmation
+    )
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
---
+++ .history/app/models/user_20210309150840.rb
@@ -0,0 +1,29 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :tasks, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }
+                    # format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
+  before_update :validate_update
+  before_destroy :validate_destroy
+  validates :password, presence: true, length: { minimum: 6 }
+
+  private
+  def validate_update
+    validate_admin_last_update
+  end
+
+  def validate_destroy
+    validate_admin_last
+  end
+
+  def validate_admin_last
+    throw(:abort) if self.admin && User.where(admin: true).length == 1
+  end
+
+  def validate_admin_last_update
+    throw(:abort) if !self.admin && self.admin_was && User.where(admin: true).length == 1
+  end
+end
