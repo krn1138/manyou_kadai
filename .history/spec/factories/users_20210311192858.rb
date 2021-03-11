bb1dfc685cd92087317834d8cbd7e4fec7c31a0f
oomiya karen <krn971138@gmail.com>
2021-03-11 19:49:06 +0900

    rspec訂正

---
+++ .history/spec/factories/users_20210311192858.rb
@@ -0,0 +1,25 @@
+FactoryBot.define do
+  factory :user do
+    name { "karen" }
+    email { "kkkaaa@example.com" }
+    password { "password1" }
+    password_confirmation { "password1" }
+    admin { false }
+  end
+
+  factory :admin_user, class: User do
+    name { "oomiya" }
+    email { "oomiya@example" }
+    password { "password2" }
+    password_confirmation { "password2" }
+    admin { true }
+  end
+
+  factory :usertwo, class: User do
+    name { "tarou" }
+    email { "tarou@example" }
+    password { "password3" }
+    password_confirmation { "password3" }
+    admin { false }
+  end
+end
